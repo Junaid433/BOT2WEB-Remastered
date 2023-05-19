69a48e63f6efe5be0b71bba04f2010553e0320c2
Junaid Rahman <rahmanjunaid226@yahoo.com>
2023-05-19 14:54:09 +0600

    Add files via upload

---
+++ api/client.py
@@ -0,0 +1,19 @@
+from pyrogram import Client
+
+client1 = Client(
+    'client1',
+    api_id = 97442,
+    api_hash = '9864bfad32514a4effdf843ad1f97'
+)
+
+client2 = Client(
+    'client2',
+    api_id = 8870,
+    api_hash = 'f7a702f90a262c83f2d5c275bb2'
+)
+
+client3 = Client(
+    'client3',
+    api_id = 1450,
+    api_hash = '3717106ec05155d52f5b'
+)
